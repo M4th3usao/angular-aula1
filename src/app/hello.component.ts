a5b2e38584ba716a340e0b62d4e48349ae62948a
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-09-02 19:45:27 -0300

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: 'hello', //nome da tag, é necessário.
+  template: `<h1>Hello {{name}}!</h1>`, // é qual é o código que está dentro dessa tag, interpolação
+  styles: [`h1 { font-family: Lato; }`], //estilos utilizados no componente
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
 }
+
+//amostra de componente
+//nome do arquivo nome.component.ts , senão vai dar erro.
