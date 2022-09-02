abf0c488bf0fc4183642fb932bca2524d613e5d8
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-09-02 19:50:09 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //apontamento para outro arquivo, no caso para o my-app
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HelloComponent } from './hello.component'; //sem extensão é TypeS
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+//nesse arquivo que insere-se os parâmetros do módulo, ou seja, separar os componentes em módulos. Cada pasta terá um arquivo de configuração
