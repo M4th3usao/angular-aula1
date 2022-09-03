1943021c1f963a8c41781574e7f473931a2522aa
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-09-02 21:47:37 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,35 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name = 'Matheus Henrique';
   nascimento = 2001;
+  action = '-';
+  round = 0;
+  second = 5;
   counter = 0;
   timer = setInterval(() => {
+    if (this.round < 1) this.action = 'PREPARE';
+    else if (this.round > 8) this.action = 'DONE!';
+    else if (this.second > 10) this.action = 'DO IT!!!';
+    else this.action = 'REST!';
+    document.getElementById('app-action').innerHTML = this.action;
+    if (this.round >= 1 && this.round <= 8) {
+      document.getElementById('app-round').innerHTML = this.round.toString();
+    } else {
+      document.getElementById('app-round').innerHTML = '-';
+    }
+    if (this.round <= 8) {
+      document.getElementById('app-time').innerHTML =
+        this.second.toString() + 's';
+    } else {
+      document.getElementById('app-time').innerHTML = '-';
+    }
     this.counter = this.counter + 1;
-  }, 100);
+
+    this.second -= 1;
+    if (this.second == 0) {
+      this.round += 1;
+      this.second = 30;
+    }
+  }, 1000);
 
   getIdade() {
     return 2022 - this.nascimento;
@@ -21,5 +46,8 @@ export class AppComponent {
   }
   getSegundos() {
     return Math.trunc(this.counter % 60);
+  }
+  getNumbers() {
+    return [1, 89, 35, 22, 17];
   }
 }
